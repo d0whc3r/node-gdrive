7aef65787c6585d847e1e13c063df87a206f5976
d0whc3r <d0whc3r@gmail.com>
2019-03-16 16:41:39 +0100

    add cli

---
+++ cli/cli.ts
@@ -0,0 +1,199 @@
+import GDrive from '@/index';
+import { options } from './cliconfig';
+import * as colors from 'colors';
+import mysqlDump from 'mysql-backup';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as moment from 'moment';
+import { Schema$File$Modded } from '../src';
+import Config from '../src/config';
+import FileUtils from '../src/file.utils';
+
+const theme = {
+  folder: 'cyan',
+  error: 'red',
+};
+
+colors.setTheme(theme);
+
+class Cli {
+  constructor(
+      private opts = options,
+      private gdrive = new GDrive()) {
+    console.log('OPTIONS', this.opts);
+  }
+
+  async parseOptions() {
+    for (let command in this.opts) {
+      const args = this.opts[command];
+      const zip = this.opts['zip'];
+      const folder = this.opts['folder'];
+      const replace = this.opts['replace'];
+      const create = this.opts['create'];
+      switch (command) {
+        case 'backup':
+          await this.backup(args, { zip, folder, replace, create });
+          break;
+        case 'delete':
+          await this.delete(args, folder);
+          break;
+        case 'mysql':
+          await this.dumpMysql({ zip, folder, replace, create });
+          break;
+        case 'list':
+          await this.showList();
+          break;
+      }
+    }
+  }
+
+  async showList() {
+    const files = (await this.gdrive.listFiles())
+        .filter((file) => !file.isDeleted);
+    if (files.length) {
+      console.log(`${Config.TAG} File list:`);
+      this.beautifulFiles(files);
+    } else {
+      console.log(`${Config.TAG} No files found.`);
+    }
+  }
+
+  async delete(args, folder) {
+    if (!Array.isArray(args)) {
+      args = [args];
+    }
+    const parsed = args.map(this.parseDelete).map((arg) => {
+      if (!arg.folder) {
+        arg.folder = folder;
+      }
+      return {
+        ...arg,
+      };
+    });
+    for (let info of parsed) {
+      const { folder, timeSpace } = info;
+      await this.gdrive.cleanOlder(timeSpace, folder);
+    }
+  }
+
+  async backup(
+      files: string | string[],
+      options: { zip: string, folder: string, replace: boolean, create: boolean },
+  ) {
+    const { zip, folder, replace, create } = options;
+    if (!Array.isArray(files)) {
+      files = [files];
+    }
+    await this.gdrive.uploadFiles(files, folder || false, {
+      compress: zip || zip === null,
+      replace,
+      create,
+    });
+  }
+
+  async dumpMysql(options: { zip: string, folder: string, replace: boolean, create: boolean }) {
+    const mysqlfile = await this.createDumpFile();
+    await this.backup(mysqlfile, options);
+  }
+
+  private parseDelete(arg) {
+    let folder;
+    let timeSpace;
+    const parsed = arg.split('=');
+    if (parsed.length > 1) {
+      folder = parsed[0];
+      timeSpace = parsed[1];
+    } else {
+      timeSpace = arg;
+      folder = null;
+    }
+    return {
+      folder,
+      timeSpace,
+    };
+  }
+
+  private findFile(files: Schema$File$Modded[], fileId: string) {
+    const result = files.find((file) => file.id === fileId);
+    if (result) {
+      return result.name;
+    } else {
+      return null;
+    }
+  }
+
+  private beautifulFiles(files: Schema$File$Modded[]) {
+    const parsed = files.map((file) => {
+      return {
+        ...file,
+        parentFolder: this.findFile(files, file.parents[0]),
+      };
+    });
+    parsed.sort(this.sortByName);
+    parsed.sort(this.sortByParent);
+    parsed.sort(this.sortByFolder);
+    parsed.forEach((file) => {
+      const fileName = file.isFolder ? this.showFolder(file.name) : file.name;
+      const parent = file.parentFolder ? this.showFolder(`${file.parentFolder}/`) : '';
+      console.log(parent + fileName);
+    });
+  }
+
+  private sortByName(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  private sortByFolder(a, b) {
+    if (a.isFolder && !b.isFolder) {
+      return -1;
+    } else if (!a.isFolder && b.isFolder) {
+      return 1;
+    }
+    return 0;
+  }
+
+  private sortByParent(a, b) {
+    if ((a.parentFolder && b.parentFolder) || (!a.parentFolder && !b.parentFolder)) {
+      return 0;
+    } else if (!a.parentFolder && b.parentFolder) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  private showFolder(name: string) {
+    return name['bold'][theme.folder];
+  }
+
+  private createDumpFile() {
+    const host = process.env.MYSQL_HOST || 'localhost';
+    const user = process.env.MYSQL_USER;
+    const password = process.env.MYSQL_PASSWORD;
+    const database = process.env.MYSQL_DATABASE;
+    if (!user || !password || !database) {
+      console.error(`${Config.TAG} Error in mysql-dump environment variables not defined`);
+      console.error('$MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_HOST, $MYSQL_PORT');
+    }
+    return mysqlDump({
+      host,
+      user,
+      password,
+      database,
+    }).then(dump => {
+      const fileDest = `./files/mysqldump-${moment().format('YYYY-MM-DD.HHmmss')}.sql`;
+      FileUtils.mkdirp(path.dirname(fileDest));
+      fs.writeFileSync(fileDest, dump);
+      return fileDest;
+    });
+  }
+}
+
+(async () => {
+  await new Cli().parseOptions();
+})();
---
+++ cli/cliconfig.ts
@@ -0,0 +1,145 @@
+import commandLineArgs = require('command-line-args');
+import commandLineUsage = require('command-line-usage');
+import { OptionDefinition, Section } from 'command-line-usage';
+import { CommandLineOptions } from 'command-line-args';
+
+const optionDefinitions: OptionDefinition[] = [
+  {
+    name: 'list',
+    alias: 'l',
+    multiple: false,
+    type: Boolean,
+    description: 'List all files and folders',
+  },
+  {
+    name: 'backup',
+    alias: 'b',
+    typeLabel: '{underline file*} ',
+    multiple: true,
+    type: String,
+    description: 'Backup files',
+  },
+  {
+    name: 'zip',
+    alias: 'z',
+    typeLabel: '{underline zipname.zip}',
+    multiple: false,
+    type: String,
+    description: 'Zip backup files',
+  },
+  {
+    name: 'replace',
+    alias: 'r',
+    multiple: false,
+    type: Boolean,
+    description: 'Replace files if already exists when backup upload',
+  },
+  {
+    name: 'create',
+    alias: 'c',
+    multiple: false,
+    defaultValue: true,
+    type: Boolean,
+    description: 'Create destination upload folder (default = true)',
+  },
+  {
+    name: 'folder',
+    alias: 'f',
+    typeLabel: '{underline foldername}',
+    multiple: false,
+    type: String,
+    description: 'Folder name to upload file/s',
+  },
+  {
+    name: 'delete',
+    alias: 'd',
+    typeLabel: '{underline foldername=duration}',
+    multiple: true,
+    type: String,
+    description: 'Clean files older than duration in foldername',
+  },
+  {
+    name: 'mysql',
+    alias: 'm',
+    multiple: false,
+    type: Boolean,
+    description: 'Mysql backup using environment variables to connect mysql server ($MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_HOST, $MYSQL_PORT)',
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    description: 'Print this usage guide.',
+    type: Boolean,
+  },
+];
+
+export interface GDriveOptions extends CommandLineOptions {
+  help?: boolean;
+  list?: boolean;
+  backup?: string | string[];
+  folder?: string;
+  zip?: string;
+  create?: boolean;
+  replace?: boolean;
+  delete?: string;
+  mysql?: boolean;
+}
+
+let options: GDriveOptions = {} as GDriveOptions;
+try {
+  options = commandLineArgs(optionDefinitions);
+} catch (e) {
+  console.error('[-] Error:', e.message);
+  process.exit(1);
+}
+
+if (options.help || !Object.keys(options).length) {
+  const ex = 'gdrive';
+  const sections: Section[] = [
+    {
+      header: `Help for ${ex}`,
+      content: `Usage of npm {italic ${ex}} in command line.`,
+    },
+    {
+      header: 'Options',
+      optionList: optionDefinitions,
+    },
+    {
+      header: 'Examples',
+      content: [
+        {
+          desc: '1. List files.',
+          example: `$ ${ex} -l`,
+        },
+        {
+          desc: '2. Backup multiple files to "backupFolder" folder.',
+          example: `$ ${ex} -b src/index.ts -b images/logo.png -f backupFolder`,
+        },
+        {
+          desc: '3. Backup files using wildcard to "backup" folder.',
+          example: `$ ${ex} -b src/* -b images/* -f backup`,
+        },
+        {
+          desc: '4. Backup files using wildcard and zip into "zipped" folder folder will be created if it doesn\'t exists.',
+          example: `$ ${ex} -b src/* -b images/* -z -f zipped.zip -c`,
+        },
+        {
+          desc: '5. Backup files using wildcard and zip using "allfiles.zip" as filename into "zipped" folder folder will be created if it doesn\'t exists and zipfile will be replaced if it exists',
+          example: `$ ${ex} -b src/* -b images/* -z allfiles.zip -f zipped -c -r`,
+        },
+        {
+          desc: '6. Delete files in "uploads" folder older than 2days and files in "monthly" folder older than 1month',
+          example: `$ ${ex} -d uploads=2d -d monthly=1M`,
+        },
+        {
+          desc: '7. Delete files in "uploads" folder older than 1minute',
+          example: `$ ${ex} -f uploads -d 1m`,
+        },
+      ],
+    },
+  ];
+  console.log(commandLineUsage(sections));
+  process.exit(1);
+}
+
+export { options };
