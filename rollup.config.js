3a80e76b87190bc153721e29c4f209ac1d66607b
d0whc3r <d0whc3r@gmail.com>
2020-07-05 10:25:44 +0200

    chore: update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,17 @@
-import typescript from 'rollup-plugin-typescript2';
+import autoExternal from 'rollup-plugin-auto-external';
+import esbuild from 'rollup-plugin-esbuild';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import findNodeModules from 'find-node-modules';
 import builtinModules from 'builtin-modules';
 import pkgJson from './package.json';
-import fs from 'fs';
-import path from 'path';
-
-let CHECKED_DEPS = [];
 
 export default function odpRollupConfig(options) {
-  const extensions = ['.ts', '.js', '.mjs'];
   const resolveOptions = {
     mainFields: ['jsnext:main', 'es2017', 'es2015', 'module', 'main'],
     preferBuiltins: true,
-    extensions,
-    modulesOnly: true,
+    extensions: ['.ts', '.js', '.mjs', '.node'],
+    modulesOnly: false,
     browser: true
   };
   const plugins = [
@@ -28,73 +22,27 @@ export default function odpRollupConfig(options) {
       namedExports: true,
       indent: '  '
     }),
-    nodeResolve(resolveOptions),
-    typescript({
-      useTsconfigDeclarationDir: false,
-      tsconfigOverride: {
-        compilerOptions: { module: 'esnext' }
-      }
+    autoExternal({
+      builtins: true,
+      peerDependencies: true,
+      dependencies: true
     }),
-    commonjs(),
-    terser()
+    nodeResolve(resolveOptions),
+    esbuild({
+      minify: true,
+      target: 'esnext'
+    }),
+    commonjs()
   ];
 
-  function getContent(dir) {
-    return JSON.parse(fs.readFileSync(dir, 'utf8'));
-  }
-
-  function getDependencies(pkg) {
-    return [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
-  }
-
-  function getAllDependencies(dep) {
-    let result = [];
-    findNodeModules()
-      .map((dir) => path.resolve(process.cwd(), dir))
-      .forEach((dir) => {
-        const depPkg = path.join(dir, dep, 'package.json');
-        if (fs.existsSync(depPkg)) {
-          const content = getContent(depPkg);
-          const deps = getDependencies(content);
-          deps.forEach((d) => {
-            if (!CHECKED_DEPS.includes(d)) {
-              CHECKED_DEPS = [...CHECKED_DEPS, d];
-              result = [...result, ...getAllDependencies(d)];
-            }
-          });
-          result = [...result, ...deps];
-        }
-      });
-    return result;
-  }
-
-  function filterDependencies(pkg, deps) {
-    const { devDependencies } = pkg;
-    let result = [...deps];
-    if (devDependencies) {
-      result = result.filter((d) => !Object.keys(devDependencies).includes(d));
-    }
-    return result.filter((dep, index, array) => array.indexOf(dep) === index);
-  }
-
-  function getRecursiveDependencies(pkg) {
-    const deps = getDependencies(pkg);
-    let result = [...deps];
-    deps.forEach((dep) => {
-      CHECKED_DEPS = [...CHECKED_DEPS, dep];
-      result = [...result, ...getAllDependencies(dep)];
-    });
-    return filterDependencies(pkg, result);
-  }
-
-  const external = [...getRecursiveDependencies(pkgJson), ...builtinModules];
+  const external = [...builtinModules];
   const outputConfig = { sourcemap: false, globals: {} };
   const output = {
     input: 'src/index.ts',
     treeshake: true,
     output: [
       { ...outputConfig, file: pkgJson.main, format: 'cjs' },
-      { ...outputConfig, file: pkgJson.module, format: 'es' }
+      { ...outputConfig, file: pkgJson.module, format: 'esm' }
     ],
     plugins,
     external,
